eadad4bf11b666ba47631743fbc128ccfac13edc
Feru <f1nandocor@gmail.com>
2022-01-19 07:54:41 -0600

    .

---
+++ 14-MissingReindeer.js
@@ -0,0 +1,11 @@
+
+console.log(missingReindeer([0, 2, 3]))
+console.log(missingReindeer([5, 6, 1, 2, 3, 7, 0]))
+console.log(missingReindeer([0, 1]))
+console.log(missingReindeer([3, 0, 1]))
+console.log(missingReindeer([9, 2, 3, 5, 6, 4, 7, 0, 1]))
+console.log(missingReindeer([0]))
+
+function missingReindeer (ids) {
+
+}
